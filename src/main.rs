4c5da89c2008474ada5a0ecf04e417acda5f3324
given <givensuman@gmail.com>
2024-05-23 19:34:53 -0400

    different approach

--- src/main.rs
+++ src/main.rs
@@ -43,3 +43,40 @@ fn main() {
 
     println!("{:?}", shifted_contents);
 }
+
+/**
+fn main() {
+    let key = String::from("LEMON");
+    let text: String = String::from("THISISTEXT");
+
+    let mut key_as_u32 = key.chars().map(|c| c.to_ascii_uppercase() as u32 - 'A' as u32).cycle();
+    
+    let text_encrypted: String = text.chars().map(|c| shift_letter(c, key_as_u32.next().unwrap())).collect();
+    println!("{:?}", text_encrypted);
+
+    let mut unkey_as_u32 = key.chars().map(|c| c.to_ascii_uppercase() as u32 - 'A' as u32).cycle();
+
+    let text_unencrypted: String = text_encrypted.chars().map(|c| unshift_letter(c, unkey_as_u32.next().unwrap())).collect();
+    println!("{:?}", text_unencrypted);
+}
+
+fn shift_letter(c: char, amount: u32) -> char {
+   let c_as_u32 = c.to_ascii_uppercase() as u32 - 'A' as u32;
+   let mut shifted_c_as_u32: u32 = c_as_u32 + amount;
+   if shifted_c_as_u32 > 25 {
+    shifted_c_as_u32 -= 26
+   }
+
+   return std::char::from_u32(shifted_c_as_u32 + 'A' as u32).unwrap();
+}
+
+fn unshift_letter(c: char, amount: u32) -> char {
+    let c_as_i32: i32 = c.to_ascii_uppercase() as i32 - 'A' as i32;
+    let mut shifted_c_as_i32: i32 = c_as_i32 - i32::try_from(amount).unwrap();
+    if shifted_c_as_i32 < 0 {
+        shifted_c_as_i32 += 26
+    }
+
+    return std::char::from_u32(u32::try_from(shifted_c_as_i32).unwrap() + 'A' as u32).unwrap();
+}
+*/
